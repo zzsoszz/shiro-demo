409eaa0de3dc98706fda91bad07d5cadacf4b125
zzsoszz <137573155@qq.com>
2017-02-03 22:25:23 +0800

    update

--- src/main/java/com/qingtian/shiro_demo/App.java
+++ src/main/java/com/qingtian/shiro_demo/App.java
@@ -1,18 +1,39 @@
 package com.qingtian.shiro_demo;
 
 import org.apache.shiro.web.servlet.AdviceFilter;
-import org.apache.shiro.web.servlet.NameableFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainer;
+import org.springframework.context.ConfigurableApplicationContext;
+import com.qingtian.shiro_demo.entity.Customer;
+import com.qingtian.shiro_demo.repository.CustomerRepository;
+
 
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class App 
 {
+	TomcatEmbeddedServletContainer ss;
+	EmbeddedServletContainerFactory ssss;
     public static void main( String[] args )
     {
-    	AdviceFilter mf;
-    	NameableFilter nf;
+		ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
+		CustomerRepository customerRepository = context.getBean(CustomerRepository.class);  
+	    // 内存数据库操作  
+		Customer customer = new Customer();
+		customer.setUsername("admin");
+		customer.setPwd("admin");
+		customer.setNickName("zrk1000");
+		customer.setTel("18888888888");
+		customer.setEmail("zrk1000@163.com");
+		customer.setHeadImg("https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=266158537,114847377&fm=116&gp=0.jpg");
+		customer.setUseable(true);
+		customerRepository.save(customer);  
+		customerRepository.findAll().stream().forEach(System.out::println);
         System.out.println( "Hello World!" );
     }
 }
--- src/main/java/com/qingtian/shiro_demo/ShiroConfig.java
+++ src/main/java/com/qingtian/shiro_demo/ShiroConfig.java
@@ -2,16 +2,28 @@ package com.qingtian.shiro_demo;
 
 import java.util.LinkedHashMap;
 
+
 import java.util.Map;
 
 import javax.servlet.Filter;
 
+import org.apache.shiro.cache.MemoryConstrainedCacheManager;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.session.mgt.eis.MemorySessionDAO;
+import org.apache.shiro.session.mgt.eis.SessionDAO;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.pac4j.core.client.Clients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import com.zrk.oauthclient.client.QqClient;
+import com.zrk.oauthclient.client.SinaWeiboClient;
+import com.zrk.oauthclient.client.WeiXinClient;
 import com.zrk.oauthclient.shiro.support.ClientFilter;
 
-
+@Configuration
 public class ShiroConfig {
 	//shiro过滤器
 	@Bean(name="shiroFilter")
@@ -39,5 +51,53 @@ public class ShiroConfig {
 		shiroFilterFactoryBean.setSuccessUrl(config.getSuccessUrl());
 		shiroFilterFactoryBean.setUnauthorizedUrl(config.getUnauthorizedUrl());
 		return shiroFilterFactoryBean;
+	}
+
+	//第三方登录client配置
+	@Bean
+	public Clients getClients(){
+		SpringShiroAutoconfig config = getShiroConfig();
+		QqClient qqClient = new QqClient(config.getQqKey(),config.getQqSecret());
+		WeiXinClient weiXinClient = new WeiXinClient(config.getWeixinKey(),config.getWeixinSecret());
+		SinaWeiboClient sinaWeiboClient = new SinaWeiboClient(config.getWeiboKey(),config.getWeiboSecret());
+		Clients clients = new Clients(config.getOauthCallback(),qqClient,weiXinClient,sinaWeiboClient);
+		return clients;
+	}
+	
+	@Bean(name="memoryCacheManager")
+	public MemoryConstrainedCacheManager getMemoryCacheManager() {
+		return new MemoryConstrainedCacheManager();
+	}
+	@Bean(name="sessionDAO")
+	public SessionDAO getSessionDAO() {
+		return new MemorySessionDAO();
+	}
+	@Bean(name="sessionManager")
+	public DefaultWebSessionManager getDefaultWebSessionManager() {
+		DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+		sessionManager.setSessionValidationSchedulerEnabled(true);
+		sessionManager.setSessionDAO(getSessionDAO());
+		sessionManager.setGlobalSessionTimeout(30 * 60 * 1000);
+		sessionManager.setSessionValidationSchedulerEnabled(true);
+		sessionManager.setCacheManager(getMemoryCacheManager());
+		return sessionManager;
+	}
+	@Bean(name = "securityManager")
+	public DefaultWebSecurityManager defaultWebSecurityManager() {
+		DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+		securityManager.setRealm(getShiroDbAndClientRealm());
+		securityManager.setCacheManager(getMemoryCacheManager());
+		securityManager.setSessionManager(getDefaultWebSessionManager());
+		return securityManager;
+	}
+	@Bean(name = "shiroDbAndClientRealm")
+	public ShiroDbAndClientRealm getShiroDbAndClientRealm(){
+		ShiroDbAndClientRealm realm = new ShiroDbAndClientRealm();
+		realm.setClients(getClients());
+		return realm;
+	}
+	@Bean
+	public SpringShiroAutoconfig getShiroConfig(){
+		return new SpringShiroAutoconfig();
 	}
 }
